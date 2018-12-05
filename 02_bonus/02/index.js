361b3370a0e362236ce4b624845b39f21bf1b043
Thibaut MELEN <thmelen@e2r2p4.42.fr>
2018-12-05 13:33:12 +0100

    chagement

--- 02_bonus/02/index.js
+++ 02_bonus/02/index.js
@@ -1,14 +1,12 @@
-//Load express module with `require` directive
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('Hello World !!!!!!!')
-})
+	app.get('/', function (req, res) {
+		res.send('Hello World!')
+	})
 
-//Launch listening server on port 8081
 app.listen(8081, function () {
-  console.log('app listening on port 8081!')
+	console.log('Example app listening on port 8081 !')
 })
-   
+
+
